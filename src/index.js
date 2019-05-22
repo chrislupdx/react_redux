06615e7da0c579acab0cd2bca9e3bee35a947216
dreamer <wildetangeant@gmail.com>
2019-05-22 16:04:56 -0700

    lunchbox directory did

--- src/index.js
+++ src/index.js
@@ -1,67 +1,54 @@
 import { createStore } from 'redux';
+import { ADD_DRINK, addDrink, 
+  ADD_SANDWICH, addSanwich, 
+  addChips, ADD_CHIPS,
+  removeDrink, REMOVE_DRINK,
+  removeSandwhich, REMOVE_SANDWHICH,
+  removeChips, REMOVE_CHIPS
+} from './lunchActions'; 
 
 const initialState = {
-  drink: [],
+  drink: ['goaturine'],
   chips: null,
   sandwich: null
 };
 
+const store = createStore(reducer);
+
+
 function reducer(state = initialState, action) {
   switch(action.type) {
-    case 'Add drink':
+    case ADD_DRINK:
       return { ...state, drink: action.payload };
-    case 'Add sandwich':
+    case ADD_SANDWICH:
       return { ...state, sandwich: action.payload };
-    case 'add chips':
+    case ADD_CHIPS:
       return { ...state, chips: action.payload };
-    case 'remove drink':
+    case REMOVE_DRINK:
       return { ...state, drink: null };
-    case 'remove sandwich':
+    case REMOVE_SANDWHICH:
       return { ...state, sandwich: null };
-    case 'remove chips':
+    case REMOVE_CHIPS:
       return { ...state, chips: null };
     default:
       return state;
   }
 }
 
-const store = createStore(reducer);
-
-store.dispatch({
-  type: 'Add drink',
-  payload: ['goatsweat ', 'jet fuel']
-});
+store.dispatch(addDrink('sorrow'));
 console.log('added drink', store.getState());
 
-
-store.dispatch({
-  type: 'Add sandwich',
-  payload: 'sorrow'
-});
+store.dispatch(addSanwich('ruin'));
 console.log('added sandwich', store.getState());
 
-store.dispatch({
-  type: 'Add_chips',
-  payload: 'cheese'
-});
+store.dispatch(addChips('longing'));
 console.log('added chips', store.getState());
 
-store.dispatch({
-  type: 'remove drink',
-  payload: 'goat sweat'
-});
+store.dispatch(removeDrink('goaturine'));
 console.log('remove drink', store.getState());
 
-store.dispatch({
-  type: 'remove sandwhich',
-  payloda: 'sorrow'
-});
+store.dispatch(removeSandwhich('ugh'));
 console.log('remove sandwhich', store.getState());
 
-store.dispatch({
-  type: 'remove chips',
-  payload: 'cheese'
-});
+store.dispatch(removeChips('fine'));
 console.log('remove chips', store.getState());
-
-console.log('last log', store.getState());
