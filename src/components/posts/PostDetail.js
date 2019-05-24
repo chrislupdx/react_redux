15cfcd2db7908e438a8c67fdea125c20685c62ee
dreamer <wildetangeant@gmail.com>
2019-05-23 17:34:55 -0700

    ui sort of works

---
+++ src/components/posts/PostDetail.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+function PostDetail({ post }) {
+  const paras = post.body.split('/n').map((p, i) => (
+    <p key={i}>{p}</p>
+  ));
+  return (
+    <section>
+      <h1>{post.title}</h1>
+      {paras}
+    </section>
+  );
+}
+
+PostDetail.PropTypes = {
+  post: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired
+  }).isRequired
+}
+;
+
+export default PostDetail;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-import { createStore } from 'redux';
-import { ADD_POST, addPost, REMOVE_POST, removePost } from './actions/postActions'; 
-import { ADD_COMMENT, addComment } from './actions/commentAction';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from './components/App';
 
-
-const store = createStore(reducer);
-
-store.dispatch(addPost({ title: 'fake post', body: 'all lies' }));
-console.log('add post', store.getState());
-
-store.dispatch(removePost({ title: null, body: null }));
-console.log('removepost post', store.getState());
-
-store.dispatch(addComment({ comment: 'comment' }));
-console.log('added a comment', store.getState());
-
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+;
