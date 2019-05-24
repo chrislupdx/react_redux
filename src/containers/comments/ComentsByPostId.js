b0c327de7c6246b6b407a845c8287833780b09e5
dreamer <wildetangeant@gmail.com>
2019-05-24 15:41:28 -0700

    comment selector written+looped into commentbypostid container

--- src/containers/comments/ComentsByPostId.js
+++ src/containers/comments/ComentsByPostId.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import Comments from '../../components/comments/Comments';
 import { deleteComment } from '../../actions/commentAction';
-
+import { getComments } from '../../selectors/commentSelectors';
 
 
 const mapStateToProps = (state, { postId }) => ({
