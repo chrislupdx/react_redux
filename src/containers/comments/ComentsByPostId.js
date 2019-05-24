1ccc93e04ebbf9de4453803053c65b76ebede66d
dreamer <wildetangeant@gmail.com>
2019-05-24 15:29:26 -0700

    Commenbypostid container written, need to introduce actions/reducers/components

---
+++ src/containers/comments/ComentsByPostId.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+
+
+const mapStateToProps = (state, { postId }) => ({
+  comments: getComments(state, postId)
+});
+
+const mapDispatchToProps = (dispatch, { postId }) => ({
+  deleteComment(commentId) {
+    dispatch(deleteComment(postId, commentId));
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Comments);
